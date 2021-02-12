111605d22dd8333c3cfce07b33123d8741c1fae3
Hsiyu Yang <hsinyuy@icloud.com>
2021-02-12 23:15:17 +0800

    Next.js Tutorial #9 - Images & Metadata

--- pages/about.js
+++ pages/about.js
@@ -1,14 +1,23 @@
+import Head from 'next/head'
+
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel error
-        debitis rerum, sed voluptas culpa, corrupti, suscipit laborum rem dolor
-        nostrum esse temporibus dolorem doloribus laudantium quisquam cupiditate
-        possimus a?
-      </p>
-    </div>
+    <>
+      <Head>
+          <title>About | Ninja List</title>
+          <meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
+          <meta name="keywords" content="ninjas"/>
+        </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel error
+          debitis rerum, sed voluptas culpa, corrupti, suscipit laborum rem dolor
+          nostrum esse temporibus dolorem doloribus laudantium quisquam cupiditate
+          possimus a?
+        </p>
+      </div>
+    </>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -6,17 +6,24 @@ import Footer from '../components/Footer'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>Homepage</h1>
-      <p className={styles.text}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore iusto
-        harum, amet voluptatum iste tenetur quisquam quam sed laudantium
-        distinctio illo debitis expedita accusamus minus veniam dolorum id?
-        Maiores, repellendus.
-      </p>
-      <Link href="/ninjas">
-        <a className={styles.btn}>See Ninja Listing</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Home | Ninja List</title>
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
+        <meta name="keywords" content="ninjas"/>
+      </Head>
+      <div className={styles.container}>
+        <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore iusto
+          harum, amet voluptatum iste tenetur quisquam quam sed laudantium
+          distinctio illo debitis expedita accusamus minus veniam dolorum id?
+          Maiores, repellendus.
+        </p>
+        <Link href="/ninjas">
+          <a className={styles.btn}>See Ninja Listing</a>
+        </Link>
+      </div>
+    </>
   )
 }
