ba667990fd92d3bf835989530b026716d9c02dd3
Hsiyu Yang <hsinyuy@icloud.com>
2021-02-12 19:51:58 +0800

    Next.js Tutorial #1, #2 - Setup, Pages & Routes

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel error
+        debitis rerum, sed voluptas culpa, corrupti, suscipit laborum rem dolor
+        nostrum esse temporibus dolorem doloribus laudantium quisquam cupiditate
+        possimus a?
+      </p>
+    </div>
+  )
+}
+
+export default About
